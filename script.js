3da71faada3d8543a72e0fa55ecadc2c7b1c466c
Polaris <108342258+Polariswright@users.noreply.github.com>
2022-09-28 00:16:28 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+function carregar() {
+    let msg = document.getElementById('msg');
+    let img = document.getElementById('imagem');
+    let data = new Date();
+    let minuto = data.getMinutes();
+    let hora = data.getHours();
+    let diaSem = data.getDate();
+    let mes = data.getMonth()
+    let ano = data.getFullYear()
+    msg.innerHTML = `Agora sao ${hora}:${minuto}. 
+    <p>Hoje eh dia ${diaSem}/${mes}/${ano}</p>`
+    if (hora >= 0 && hora < 12){
+        //bom dia
+        img.src = 'manha.jpg'
+
+    }
+    else if(hora >= 12 && hora < 18){
+        //boa tarde
+        img.src ='tarde.jpg'
+    }
+    else{
+        img.src ='noite.jpg'
+    }
+
+    
+}
